d71cf52f35f1a753259b6ba12fe24892bd1909f3
Luke Kaalim <lukekaalim@gmail.com>
2018-11-19 05:18:17 +1100

    remove overcomplicated config values

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 // @flow
-import type { LoggingServiceConfig } from './services/logging';
-import type { LocalStorageServiceConfig } from './services/storage/localStorage';
-
 import { resolve } from 'path';
 
 export type Environment = 'local' | 'test' | 'dev' | 'uat' | 'qa' | 'stag' | 'prod';
@@ -9,19 +6,13 @@ export type Environment = 'local' | 'test' | 'dev' | 'uat' | 'qa' | 'stag' | 'pr
 export type ScribeConfig = {
   environment: Environment,
   services: {
-    logging: LoggingServiceConfig,
-    localStorage: LocalStorageServiceConfig,
+    storageRootDirectory: string,
   },
 };
 
 export const LOCAL_CONFIG: ScribeConfig = {
   environment: 'local',
   services: {
-    logging: {
-      mode: 'stdout',
-    },
-    localStorage: {
-      storageRootDirectory: resolve(__dirname, '../temp'),
-    }
-  }
+    storageRootDirectory: resolve(__dirname, '../temp'),
+  },
 };
