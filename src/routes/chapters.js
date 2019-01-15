49e60f5af4fd6d9519784226991e2dec09a93b9c
Luke Kaalim <lukekaalim@gmail.com>
2019-01-16 05:29:18 +1100

    rename the scribe directory to atlas

--- src/routes/chapters.js
+++ src/routes/chapters.js
@@ -1,5 +1,5 @@
 // @flow
-import type { ChapterService } from '../services/scribe/chapters';
+import type { ChapterService } from '../services/atlas/chapters';
 import type { UserService } from '../services/user';
 import { buildApiRoutes, ok } from '../lib/apiRoute';
 import { toChapterId } from '../lib/chapter';
