c6f697f34ec2ac85e8e475a1fdb9f3ccf0575eff
Luke Kaalim <lukekaalim@gmail.com>
2019-01-16 05:30:56 +1100

    actually add useful role info in model

--- src/lib/role.js
+++ src/lib/role.js
@@ -1,24 +1,26 @@
 // @flow
 import type { UUID } from './uuid';
+import type { UserID } from './user';
 import type { PermissionID } from './permission';
 import { toObject } from './serialization';
 import { toUUID, generateUUID } from './uuid';
+import { toUserId } from './user';
 import { toPermissionId } from './permission';
 
 export opaque type RoleID: UUID = UUID;
 
 export type Role = {
   id: RoleID,
-  readPermission: PermissionID,
-  editPermission: PermissionID,
+  userId: UserID,
+  permissionId: PermissionID,
 };
 
 export const toRoleId = (mixed: mixed): RoleID => toUUID(mixed);
 
 export const toRole = (mixed: mixed): Role => toObject(mixed, object => ({
   id: toRoleId(object.id),
-  readPermission: toPermissionId(object.readPermission),
-  editPermission: toPermissionId(object.editPermission),
+  userId: toUserId(object.userId),
+  permissionId: toPermissionId(object.permissionId),
 }));
 
 export const generateRole = () => ({
