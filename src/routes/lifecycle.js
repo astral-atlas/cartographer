e19fa48c8efec57e3dd0c82873ea1dc4e6186acd
Luke Kaalim <lukekaalim@gmail.com>
2019-01-16 02:02:30 +1100

    Fix unneeded interpolated string backticks

--- src/routes/lifecycle.js
+++ src/routes/lifecycle.js
@@ -9,7 +9,7 @@ export const buildFallbackRoute = ({ log }: Logger<string>) => [
     handler: (inc: IncomingMessage, res: ServerResponse) => {
       log(
         `Served URL\n${inc.url}\n${inc.method}\nwith 404 - Resource Not `+
-        `Found\n This is because the fallback route responded`
+        'Found\n This is because the fallback route responded'
       );
       res.statusCode = 404;
       res.write('404 - Resource Not Found');
