a05edf1ec33e3146f78ca522d297aecac79c8687
Luke Kaalim <lukekaalim@gmail.com>
2019-01-16 05:31:37 +1100

    implement basic application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,47 @@
 // @flow
 import type { Route } from './lib/http';
+import type { RoleID, Role } from './lib/role';
+import { toPermissionId } from './lib/permission';
+import { generateUser } from './lib/user';
+
+import { buildMemoryStorageService } from './services/storage/memoryStorage';
+import { buildBasicPermissionService } from './services/permission/basicPermission';
+import { buildChapterService } from './services/atlas/chapters';
+import { buildBasicUserService } from './services/user/basicUser';
 
 import { buildChaptersRoutes } from './routes/chapters';
 
 export const buildAppRoutes = async (): Promise<Array<Route>> => {
+  const chapterStorage = buildMemoryStorageService();
 
-  return buildChaptersRoutes(chapterService, userService)
+  const roleStorage = buildMemoryStorageService<RoleID, Role>();
+
+  const getUserRoles = async (userId) => (
+    [...roleStorage.entries()]
+      .filter(([, role]) => role.userId === userId)
+      .map(([roleId]) => roleId)
+  );
+  
+  const getPermissionRoles = async (permissionId) => (
+    [...roleStorage.entries()]
+      .filter(([, role]) => role.permissionId === permissionId)
+      .map(([roleId]) => roleId)
+  );
+
+  const permissionService = buildBasicPermissionService(getUserRoles, getPermissionRoles);
+
+  const getChaptersByReadPermissions = async (userId) => {
+    const userRoleIds = await getUserRoles(userId);
+    const userRoles = await Promise.all(userRoleIds.map(roleStorage.read));
+    const readableChapters = [...chapterStorage.entries()]
+      .filter(([,chapter]) => userRoles.find(role => role.permissionId === chapter.readPermission))
+      .map(([,chapter]) => chapter);
+    return readableChapters;
+  };
+
+  const userService = buildBasicUserService(generateUser());
+
+  const chapterService = buildChapterService(chapterStorage, permissionService, toPermissionId('000-000'), getChaptersByReadPermissions);
+
+  return buildChaptersRoutes(chapterService, userService);
 };
