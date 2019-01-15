a76aa74c73cd156979e8d72a17b8c92753732286
Luke Kaalim <lukekaalim@gmail.com>
2019-01-16 01:44:32 +1100

    Add stream helper functions

---
+++ src/lib/stream.js
@@ -0,0 +1,30 @@
+// @flow
+import { Readable } from 'stream';
+
+/**
+ * Helper function for reading streams into a string
+ */
+export const readStream = (stream: Readable): Promise<string> => new Promise((resolve, reject) => {
+  const collectedChunks = [];
+  stream.setEncoding('utf8'); // (-_-) dunno bout this chief
+  stream.on('data', chunk => collectedChunks.push(chunk));
+  stream.on('end', () => resolve(collectedChunks.join('')));
+  stream.on('error', reject);
+});
+
+export class StringStream extends Readable {
+  sourceBuffer: Buffer;
+
+  constructor(source: string) {
+    super();
+    this.sourceBuffer = Buffer.from(source);
+  }
+
+  _read() {
+    this.push(this.sourceBuffer);
+  }
+
+  getLength() {
+    return this.sourceBuffer.byteLength;
+  }
+}
