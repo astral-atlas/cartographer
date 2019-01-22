cc9d85ebfe990d25856a184756d3d19afa582b3c
Luke Kaalim <lukekaalim@gmail.com>
2019-01-23 09:58:39 +1100

    implement in-memory implementation of a role service backed by a memory store

--- src/services/role/basicRole.js
+++ src/services/role/basicRole.js
@@ -1,25 +1,66 @@
 // @flow
 import type { RoleService } from '../role';
-import type { StorageService } from '../storage';
+import type { MemoryStorageService } from '../storage/memoryStorage';
 import type { RoleID, Role } from '../../lib/role';
 import type { UserID } from '../../lib/user';
 import type { PermissionID } from '../../lib/permission';
+import { generateNewRole } from '../../lib/role';
 
-export const buildBasicRoleService = (
-  roleStorageService: StorageService<RoleID, Role>,
-  getUserRoles: (userId: UserID) => Promise<Array<RoleID>>,
-  getPermissionRoles: (permissionId: PermissionID) => Promise<Array<RoleID>>,
+type RoleMemoryStructure = Role & {
+  permissionIds: Array<PermissionID>,
+  userIds: Array<UserID>,
+};
+
+export const buildMemoryRoleService = (
+  roleStorageService: MemoryStorageService<RoleID, RoleMemoryStructure>,
 ): RoleService => {
-  const getRolesForUser = getUserRoles;
-  const getRolesForPermission = getPermissionRoles;
+  const getRolesForUser = async (userId) => (
+    [...roleStorageService.entries()]
+      .filter(([,role]) => role.userIds.includes(userId))
+      .map(([roleId]) => roleId)
+  );
+  const getRolesForPermission = async (permissionId) => (
+    [...roleStorageService.entries()]
+      .filter(([,role]) => role.permissionIds.includes(permissionId))
+      .map(([roleId]) => roleId)
+  );
+  const getIntersectingRolesForUserAndPermission = async (userId, permissionId) => {
+    const [permissionRoles, userRoles] = await Promise.all([
+      getRolesForPermission(permissionId),
+      getRolesForUser(userId),
+    ]);
+
+    return userRoles.filter(userRole => permissionRoles.includes(userRole));
+  };
+
+  const addUserToRole = async (userId, roleId) => {
+    const role = await roleStorageService.read(roleId);
+    roleStorageService.update(roleId, {
+      ...role,
+      userIds: [...role.userIds, userId],
+    });
+  };
 
   const addPermissionToRole = async (permissionId, roleId) => {
-    
+    const role = await roleStorageService.read(roleId);
+    roleStorageService.update(roleId, {
+      ...role,
+      permissionIds: [...role.permissionIds, permissionId],
+    });
+  };
+
+  const addRole = async () => {
+    const newRole = generateNewRole();
+    roleStorageService.create(newRole.id, { ...newRole, permissionIds: [], userIds: [] });
+    return newRole;
   };
 
   return {
     getRolesForUser,
     getRolesForPermission,
+    getIntersectingRolesForUserAndPermission,
+    addUserToRole,
     addPermissionToRole,
+    addRole,
   };
 };
