2a8ff601b7bb9810122db59db99752d0c134e07b
Luke Kaalim <lukekaalim@gmail.com>
2019-01-22 18:16:41 +1100

    Add more controls to the role service

--- src/services/role.js
+++ src/services/role.js
@@ -1,11 +1,13 @@
 // @flow
 import type { UserID } from '../lib/user';
-import type { RoleID } from '../lib/role';
+import type { RoleID, Role } from '../lib/role';
 import type { PermissionID } from '../lib/permission';
 
 export type RoleService = {
-  getUserRoles: (userId: UserID) => Promise<Array<RoleID>>,
-  getValidRolesForPermission: (userId: UserID, permissionId: PermissionID) => Promise<Array<RoleID>>,
+  getRolesForUser: (userId: UserID) => Promise<Array<RoleID>>,
+  getRolesForPermission: (permissionId: PermissionID) => Promise<Array<RoleID>>,
+  getIntersectingRolesForUserAndPermission: (userId: UserID, permissionId: PermissionID) => Promise<Array<RoleID>>,
   addUserToRole: (userId: UserID, roleId: RoleID) => Promise<void>,
   addPermissionToRole: (permissionId: PermissionID, roleId: RoleID) => Promise<void>,
+  addRole: () => Promise<Role>,
 };
