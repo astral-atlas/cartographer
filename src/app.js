552154537ae2fc6996dccf45af8a5f123ded5203
Luke Kaalim <lukekaalim@gmail.com>
2019-01-23 18:56:58 +1100

    Refactor App.js to do less work

--- src/app.js
+++ src/app.js
@@ -1,52 +1,36 @@
 // @flow
 import type { Route } from './lib/http';
-import type { RoleID, Role } from './lib/role';
-import type { PermissionID, Permission } from './lib/permission';
-import { buildNewPermission } from './lib/permission';
+import type { Chapter } from './lib/chapter';
 import { generateUser } from './lib/user';
+import { getTupleSecond } from './lib/tuple';
+import { buildBasicPermissionIndex } from './lib/permission';
 
 import { buildMemoryStorageService } from './services/storage/memoryStorage';
 import { buildBasicPermissionService } from './services/permission/basicPermission';
 import { buildChapterService } from './services/atlas/chapters';
 import { buildBasicUserService } from './services/user/basicUser';
+import { buildMemoryRoleService } from './services/role/basicRole';
 
 import { buildChaptersRoutes } from './routes/chapters';
 
 export const buildAppRoutes = async (): Promise<Array<Route>> => {
   const chapterStorage = buildMemoryStorageService();
-  const roleStorage = buildMemoryStorageService<RoleID, Role>();
-  const permissionStorage = buildMemoryStorageService<PermissionID, Permission>();
-
-  const getUserRoles = async (userId) => (
-    [...roleStorage.entries()]
-      .filter(([, role]) => role.userId === userId)
-      .map(([roleId]) => roleId)
-  );
-  
-  const getPermissionRoles = async (permissionId) => (
-    [...roleStorage.entries()]
-      .filter(([, role]) => role.permissionId === permissionId)
-      .map(([roleId]) => roleId)
-  );
-
-  const permissionService = buildBasicPermissionService(permissionStorage, getUserRoles, getPermissionRoles);
-
-  const getChaptersByReadPermissions = async (userId) => {
-    const userRoleIds = await getUserRoles(userId);
-    const userRoles = await Promise.all(userRoleIds.map(roleStorage.read));
-    const readableChapters = [...chapterStorage.entries()]
-      .filter(([,chapter]) => userRoles.find(role => role.permissionId === chapter.readPermission))
-      .map(([,chapter]) => chapter);
-    return readableChapters;
-  };
-
+  const permissionService = buildBasicPermissionService(buildMemoryStorageService());
+  const roleService = buildMemoryRoleService(buildMemoryStorageService());
   const userService = buildBasicUserService(generateUser());
+
+  const getChaptersByReadPermissions = buildBasicPermissionIndex<Chapter>(
+    async () => [...chapterStorage.entries()].map(getTupleSecond),
+    chapter => chapter.readPermission,
+    roleService.getIntersectingRolesForUserAndPermission,
+  );
 
   const chapterService = buildChapterService(
     chapterStorage,
+    roleService,
     permissionService,
-    buildNewPermission(),
-    getChaptersByReadPermissions
+    (await permissionService.addNewPermission()).id,
+    getChaptersByReadPermissions,
   );
 
   return buildChaptersRoutes(chapterService, userService);
