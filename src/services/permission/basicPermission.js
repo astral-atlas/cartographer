f2b3f1c7ef2e0e14e64487c70a517bbd1e4aafc3
Luke Kaalim <lukekaalim@gmail.com>
2019-01-23 12:39:10 +1100

    remove unneeded implementations from basic permissions
    since the permission service got nerfed

--- src/services/permission/basicPermission.js
+++ src/services/permission/basicPermission.js
@@ -1,30 +1,18 @@
 // @flow
 import type { PermissionService } from '../permission';
-import type { UserID } from '../../lib/user';
 import type { PermissionID, Permission } from '../../lib/permission';
-import type { RoleID } from '../../lib/role';
 import type { StorageService } from '../storage';
 import { buildNewPermission } from '../../lib/permission';
 
 export const buildBasicPermissionService = (
   permissionStorageService: StorageService<PermissionID, Permission>,
-  getUserRoles: (userId: UserID) => Promise<Array<RoleID>>,
-  getPermissionRoles: (permissionId: PermissionID) => Promise<Array<RoleID>>,
 ): PermissionService => {
-  const getRolesForPermission = async (userId, permissionId) => {
-    const userRolesIds = await getUserRoles(userId);
-    const permissionRolesIds = await getPermissionRoles(permissionId);
-
-    const roleIds = userRolesIds.filter(roleId => permissionRolesIds.includes(roleId));
-    return roleIds;
-  };
   const addNewPermission = async () => {
     const newPermission = buildNewPermission();
     permissionStorageService.create(newPermission.id, newPermission);
     return newPermission;
   };
   return {
-    getRolesForPermission,
     addNewPermission,
   };
 };
