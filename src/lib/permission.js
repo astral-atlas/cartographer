8b99fd12bd99ad1232c62018939b83684baca114
Luke Kaalim <lukekaalim@gmail.com>
2019-01-25 22:21:45 +1100

    type export stricter to resolve flow inference issues

--- src/lib/permission.js
+++ src/lib/permission.js
@@ -25,7 +25,7 @@ export const buildBasicPermissionIndex = <T>(
   getItems: () => Promise<Array<T>>,
   getPermissionId: (item: T) => PermissionID,
   getUserRoleIntersection: (userId: UserID, permissionId: PermissionID) => Promise<Array<RoleID>>,
-) => async (userId: UserID): Promise<Array<T>> => {
+): (userId: UserID) => Promise<Array<T>>  => async (userId) => {
     const items = await getItems();
     const permissionIds = items.map<PermissionID>(getPermissionId);
     const roleIntersections = await Promise.all(permissionIds.map<Promise<Array<RoleID>>>(permissionId => (
