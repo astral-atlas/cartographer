632f7ee9c7bc0df81ff87deb34a05e33241e3831
Luke Kaalim <lukekaalim@gmail.com>
2019-02-04 18:28:34 +1100

    refactor permission service to use renamed function

--- src/services/permission/basicPermission.js
+++ src/services/permission/basicPermission.js
@@ -2,13 +2,13 @@
 import type { PermissionService } from '../permission';
 import type { PermissionID, Permission } from '../../lib/permission';
 import type { StorageService } from '../storage';
-import { buildNewPermission } from '../../lib/permission';
+import { generatePermission } from '../../lib/permission';
 
 export const buildBasicPermissionService = (
   permissionStorageService: StorageService<PermissionID, Permission>,
 ): PermissionService => {
   const addNewPermission = async () => {
-    const newPermission = buildNewPermission();
+    const newPermission = generatePermission();
     permissionStorageService.create(newPermission.id, newPermission);
     return newPermission;
   };
