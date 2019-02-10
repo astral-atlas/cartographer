a87d8503241809f5cd71b7e098299d1dc98d4141
Luke Kaalim <lukekaalim@gmail.com>
2019-02-11 00:20:43 +1100

    Update chapter events to check for parent chapter permission before adding event

--- src/services/atlas/chapterEvents.js
+++ src/services/atlas/chapterEvents.js
@@ -1,24 +1,45 @@
 // @flow
+import type { StorageService } from '../storage';
+import type { ChapterService } from './chapters';
+import type { RoleService } from '../role';
+import type { PermissionService } from '../permission';
+
 import type { ChapterID } from '../../models/atlas/chapter';
+import type { UserID } from '../../lib/user';
+import type { PermissionID } from '../../lib/permission';
+
 import type { ChapterEvent } from '../../models/atlas/chapter/chapterEvent';
 import type { NarrateEvent, NarrateEventID } from '../../models/atlas/chapter/narrateEvent';
-import type { StorageService } from '../storage';
+
 import { buildNarrateEvent } from '../../models/atlas/chapter/narrateEvent';
+import { InsufficientPermissionsError } from './chapters';
+import { userHasPermission } from '../role';
 
 export type ChapterEventService = {
-  getEvents: (chapterId: ChapterID) => Promise<Array<ChapterEvent>>,
-  addNarrateEvent: (chapterId: ChapterID, narration: string) => Promise<NarrateEvent>,
+  getEvents: (userId: UserID, chapterId: ChapterID) => Promise<Array<ChapterEvent>>,
+  addNarrateEvent: (userId: UserID, chapterId: ChapterID, narration: string) => Promise<NarrateEvent>,
 };
 
 export const buildChapterEventService = (
+  chapterService: ChapterService,
+  roleService: RoleService,
   narrateEventStorage: StorageService<NarrateEventID, NarrateEvent>,
   narrateEventByChapterIdIndex: (chapterId: ChapterID) => Promise<Array<NarrateEvent>>,
 ): ChapterEventService => {
-  const getEvents = async (chapterId) => [
-    ...await narrateEventByChapterIdIndex(chapterId),
-  ];
+  const getEvents = async (userId, chapterId) => {
+    // TODO: How to test for permission without just trying to grab it?
+    await chapterService.getChapter(userId, chapterId);
+    return [
+      ...await narrateEventByChapterIdIndex(chapterId),
+    ];
+  };
 
-  const addNarrateEvent = async (chapterId, narration) => {
+  const addNarrateEvent = async (userId, chapterId, narration) => {
+    const chapter = await chapterService.getChapter(userId, chapterId);
+    if (!userHasPermission(roleService, userId, chapter.masterPermission)) {
+      throw new InsufficientPermissionsError('User does not have Master permissions on chapter');
+    }
+
     const event = buildNarrateEvent(chapterId, narration);
     await narrateEventStorage.create(event.id, event);
     return event;
