59d5b76f53bec4c5c6418a4eede14bb495241a9f
Luke Kaalim <lukekaalim@gmail.com>
2019-02-11 00:21:01 +1100

    Clean up old error classes from prior to refactor

--- src/routes/routeErrors.js
+++ src/routes/routeErrors.js
@@ -1,20 +1,13 @@
 // @flow
-import type { ServerResponse } from 'http';
 import { AtlasScribeError } from '../errors';
 
-/**
- * A generic error for a route. These routes are inherently uninfomrmative,
- * and are used to send a message back to the client. They should not expose any
- * private or important information, only generic top level description of what
- * happened
- */
-export class RouteError extends AtlasScribeError {
+export class POSTInputError extends AtlasScribeError {
   constructor(message: string) {
     super(message);
   }
 }
 
-export class POSTInputError extends AtlasScribeError {
+export class URLQueryError extends AtlasScribeError {
   constructor(message: string) {
     super(message);
   }
