fe8c41b2bd7ca0c0e434546d7600aee898b16198
Luke Kaalim <lukekaalim@gmail.com>
2019-02-14 18:51:07 +1100

    Re-name components/app.js => AtlasQuill.js

--- atlas-quill/main.js
+++ atlas-quill/main.js
@@ -1,5 +1,5 @@
 import { render, jsx } from './lib/react.js';
-import { AtlasQuill } from './components/App.js';
+import { AtlasQuill } from './components/AtlasQuill.js';
 
 export const runAtlasQuill = (
   rootElement = document.body.appendChild(document.createElement('span')),
