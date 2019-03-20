e91c6704c4d66e9419d5e47643f09237f8417017
Luke Kaalim <lukekaalim@gmail.com>
2019-03-21 02:21:39 +1100

    re-implement apiRoute file as small, modular utility libraries

---
+++ src/lib/routeHandlerOutput.js
@@ -0,0 +1,49 @@
+// @flow
+import type { HandlerOutput } from './routeHandler';
+import { StringStream, NullStream } from './stream';
+import { createETag } from './etag';
+
+export const addHeaders = (out: HandlerOutput, newHeaders: Map<string, string>): HandlerOutput => ({
+  ...out,
+  headers: new Map([...out.headers, ...newHeaders]),
+});
+
+export const ok = (response: mixed): HandlerOutput => {
+  const stringifiedResponse = JSON.stringify(response);
+  const responseBody = new StringStream(stringifiedResponse);
+  const status = 200;
+  const headers = new Map([
+    ['Content-Type', 'application/json'],
+    ['Content-Length', responseBody.getLength().toString(10)],
+    ['ETag', createETag(stringifiedResponse)],
+  ]);
+  return {
+    responseBody,
+    status,
+    headers,
+  };
+};
+
+export const notAuthorized = (): HandlerOutput => ({
+  responseBody: new NullStream(),
+  status: 401,
+  headers: new Map([['Content-Length', '0']]),
+});
+
+export const notFound = (): HandlerOutput => ({
+  responseBody: new NullStream(),
+  status: 404,
+  headers: new Map([['Content-Length', '0']]),
+});
+
+export const internalServerError = (): HandlerOutput => ({
+  responseBody: new NullStream(),
+  status: 500,
+  headers: new Map([['Content-Length', '0']]),
+});
+
+export const badInput = (): HandlerOutput => ({
+  responseBody: new NullStream(),
+  status: 400,
+  headers: new Map([['Content-Length', '0']]),
+});
