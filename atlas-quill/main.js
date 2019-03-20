589b1e0f600c8759b97d6af0963871923cc6c4db
Luke Kaalim <lukekaalim@gmail.com>
2019-03-21 02:27:42 +1100

    Pass a instance of the atlas stream client to atlas quill

--- atlas-quill/main.js
+++ atlas-quill/main.js
@@ -1,8 +1,12 @@
 import { render, jsx } from './lib/react.js';
 import { AtlasQuill } from './components/AtlasQuill.js';
+import { createAtlasStreamClient } from './services/atlasStreamClient.js';
+import { createAtlasClient } from './services/atlasClient.js';
+
+const atlasStreamClient = createAtlasStreamClient(createAtlasClient('http://localhost:8888'));
 
 export const runAtlasQuill = (
   rootElement = document.body.appendChild(document.createElement('span')),
 ) => {
-  return render(jsx`<${AtlasQuill} />`, rootElement);
+  return render(jsx`<${AtlasQuill} streamClient=${atlasStreamClient} />`, rootElement);
 };
