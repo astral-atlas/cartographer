9c4776b083c2959f3d93b5da8ba9a5f74a0b3a6a
Luke Kaalim <lukekaalim@gmail.com>
2019-03-24 02:06:04 +1100

    Add basic hook exports to react lib

--- atlas-quill/lib/react.js
+++ atlas-quill/lib/react.js
@@ -2,12 +2,14 @@ import React from 'https://dev.jspm.io/react';
 import ReactDOM from 'https://dev.jspm.io/react-dom';
 import htm from 'https://dev.jspm.io/htm';
 
-const { Component, Fragment, createContext } = React;
+const { Component, Fragment, createContext, useState, useEffect } = React;
 const { render } = ReactDOM;
 const jsx = htm.bind(React.createElement);
 
 export {
   render,
+  useState,
+  useEffect,
   Component,
   Fragment,
   jsx,
