98ec4c79d64ce38b9cef83c0a84b7bc75c4b53cf
Luke Kaalim <lukekaalim@gmail.com>
2019-03-24 02:07:59 +1100

    Refactor Atlas Quill into using hooks

--- atlas-quill/components/AtlasQuill.js
+++ atlas-quill/components/AtlasQuill.js
@@ -1,7 +1,8 @@
-import { Component, jsx } from '../lib/react.js';
+import { jsx, useState, useEffect } from '../lib/react.js';
 import { css } from '../lib/style.js';
 
-import { QuillHorizontalMenu } from './QuillHorizontalMenu.js';
+import { MetaList } from './MetaList.js';
+import { ArraySelect, ArrayHeader, ArrayInsert } from './ArraySelect.js';
 
 css`
   .atlas-quill {
@@ -16,49 +17,66 @@ css`
   }
 `;
 
-export class AtlasQuill extends Component {
-  state = {
-    users: [],
-    selectedUserIndex: -1,
-  };
-  usersUnsubscribe = null;
+const useUserStream = (streamClient) => {
+  const [users, setUsers] = useState([]);
 
-  componentDidMount() {
-    this.usersUnsubscribe = this.props.streamClient.addUsersListener(users => this.onUsersUpdate(users));
-  }
+  useEffect(() => streamClient.addUsersListener(setUsers), [streamClient]);
 
-  componentWillUnmount() {
-    this.usersUnsubscribe();
-  }
+  return users;
+};
 
-  onUsersUpdate(users) {
-    if (users !== this.state.users) {
-      this.setState(state => {
-        const selectedUser = state.users[state.selectedUserIndex];
-        const newSelectedUserIndex = selectedUser && users.findIndex(user => user.id === selectedUser.id);
-        return {
-          ...state,
-          users,
-          selectedUserIndex: newSelectedUserIndex
-        };
-      });
-    }
-  }
+const useChapterStream = (streamClient, user) => {
+  const [chapters, setChapters] = useState([]);
+  useEffect(() => (
+    user && streamClient.addChaptersListener(setChapters, user.id)
+  ), [streamClient, user]);
 
-  render() {
-    const selectUser = (selectedUserIndex) => (
-      this.setState(state => ({ ...state, selectedUserIndex }))
-    );
+  return chapters;
+};
 
-    return jsx`
-      <main className="atlas-quill">
-        <h1 className="atlas-quill-heading">Atlas Quill<//>
-        <${QuillHorizontalMenu}
-          users=${this.state.users}
-          selectedUserIndex=${this.state.selectedUserIndex}
-          selectUser=${selectUser}
-        />
-      <//>
-    `;
-  }
-}
+export const AtlasQuill = ({ streamClient, client }) => {
+  const users = useUserStream(streamClient);
+  const [selectedUserId, selectUserId] = useState(null);
+  const selectedUserIndex = users.findIndex(user => user.id === selectedUserId);
+  const selectedUser = users[selectedUserIndex];
+  const chapters = useChapterStream(streamClient, selectedUser);
+
+  const userListElement = jsx`
+    <${ArrayHeader} key="0" headerText="Users" isValidItemSelected=${!!selectedUser} />
+    <${ArraySelect}
+      key="1"
+      items=${users.map(user => user.name)}
+      selectedItemIndex=${selectedUserIndex}
+      onItemSelect=${(selectedUserIndex) => selectUserId(users[selectedUserIndex].id)} 
+    />
+  `;
+  const chapterListElement = jsx`
+    <${ArrayHeader}
+      key="0"
+      headerText="Chapters"
+      isValidItemSelected=${false}
+    />
+    ${selectedUser && jsx`
+      <${ArrayInsert}
+        key="1"
+        labelText="Add New Chapter"
+        onInsert=${chapterName => client.putNewChapter(chapterName, selectedUser.id)}
+      />
+    `}
+    <${ArraySelect}
+      key="2"
+      items=${chapters.map(chapter => chapter.name)}
+      selectedItemIndex=${-1}
+      onItemSelect=${() => {}} 
+    />
+  `;
+
+  const listElements = [userListElement, chapterListElement];
+
+  return jsx`
+    <main className="atlas-quill">
+      <h1 className="atlas-quill-heading">Atlas Quill<//>
+      <${MetaList} listElements=${listElements} />
+    <//>
+  `;
+};
