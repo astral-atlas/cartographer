985243c5d0e5667aaeaff958adaa40e78d909753
Luke Kaalim <lukekaalim@gmail.com>
2019-03-25 00:07:55 +1100

    Edit Description component to allow mutiple details

--- atlas-quill/components/UserDescription.js
+++ atlas-quill/components/UserDescription.js
@@ -7,16 +7,31 @@ css`
   }
   .description-heading {
     text-align: center;
+    margin: 0.5em;
   }
   .description-text {
     text-align: center;
-    padding: 1em;
+    margin: 0.5em;
+  }
+  .description-detail {
+    list-style-type: none;
+    padding: 0;
+  }
+  .description-list {
+    margin: 0;
+    padding: 0;
   }
 `;
 
-export const UserDescription = ({ user = '' }) => jsx`
+export const Description = ({ details }) => jsx`
   <article className="description-article">
-    <h3 className="description-heading">${user}</h3>
-    <p className="description-text">A user of the site.</p>
+    <ul className="description-list">
+      ${details.map(([detailName, detailValue]) => jsx`
+      <li className="description-detail" key=${detailName}>
+        <h4 className="description-heading">${detailName}</h4>
+        <p className="description-text">${detailValue}</p>
+      </li>
+      `)}
+    </ul>
   </article>
 `;
