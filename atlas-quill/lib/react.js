f496d12864696672cb4d9b3dc532c6fe7daa1505
Luke Kaalim <lukekaalim@gmail.com>
2019-03-26 00:05:41 +1100

    export use context

--- atlas-quill/lib/react.js
+++ atlas-quill/lib/react.js
@@ -2,7 +2,7 @@ import React from 'https://dev.jspm.io/react';
 import ReactDOM from 'https://dev.jspm.io/react-dom';
 import htm from 'https://dev.jspm.io/htm';
 
-const { Component, Fragment, createContext, useState, useEffect } = React;
+const { Component, Fragment, createContext, useState, useEffect, useContext } = React;
 const { render } = ReactDOM;
 const jsx = htm.bind(React.createElement);
 
@@ -10,6 +10,7 @@ export {
   render,
   useState,
   useEffect,
+  useContext,
   Component,
   Fragment,
   jsx,
