46a01476ffcbc977532fe249151bca093143668a
Luke Kaalim <lukekaalim@gmail.com>
2019-03-31 02:21:37 +1100

    Add comment to indexer

--- src/lib/indexer.js
+++ src/lib/indexer.js
@@ -3,6 +3,15 @@ import { pFilter } from './promise';
 
 export type Indexer<TItem, TSearch> = (index: TSearch) => Promise<Array<TItem>>;
 
+/**
+ * 
+ * @param {Function} getItems
+ *  Get a list of items synchronously
+ * @param {Function} getItemIndex 
+ *  From an item, get a unique value (an 'index')
+ * @param {Function} testIndex 
+ *  Test an 'index' against a user input
+ */
 export const buildMemoryIndexer = <TItem, TIndex, TSearch>(
   getItems: () => Array<TItem>,
   getItemIndex: (item: TItem) => TIndex,
