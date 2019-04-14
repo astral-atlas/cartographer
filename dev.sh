53a42109139c81e88ddb30875a01c2409bba7aeb
Luke Kaalim <lukekaalim@gmail.com>
2019-04-15 04:03:54 +1000

    add mouse support and proper termination to dev script

--- dev.sh
+++ dev.sh
@@ -2,5 +2,6 @@
 tmux new-session -d -s atlas-dev-session watch make \; \
 split-window -d -v npx serve atlas-quill \; \
 split-window -d -h npx nodemon --signal SIGHUP dist  \; \
-set-hook pane-exited "kill-server"
+set-option mouse on  \; \
+set-hook pane-exited "kill-session -t atlas-dev-session"
 tmux attach
