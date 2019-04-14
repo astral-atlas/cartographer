040e97b5871f11f715da0b7046034aa14ea05874
Luke Kaalim <lukekaalim@gmail.com>
2019-04-15 04:05:05 +1000

    Add some console logs to describe server startup state

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { buildListener } from './listener';
 
 const main = async (environment: string) => {
   const config = getConfig(environment);
+  console.log(`Initializing Atlas Scribe in configuration ${environment}`);
   const listener = await buildListener();
   const server = createServer(listener);
-  server.listen(config.port);
+  server.listen(config.port, () => console.log(`Server is now listening on port ${config.port}`));
 };
 
 // flowlint-next-line sketchy-null-string:off
