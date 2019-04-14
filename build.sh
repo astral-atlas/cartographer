8391e83eed282352d45f28c5853898f75874a812
Luke Kaalim <lkaalim@nine.com.au>
2019-04-15 02:01:32 +1000

    Replace old build script with docker build command and dockerfile

--- build.sh
+++
@@ -1,1 +0,0 @@
-node_modules/.bin/babel src -s -d dist -w & node_modules/.bin/nodemon --inspect ./dist
---
+++ ci/docker-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mkdir -p docker-temp
+
+cp package.json docker-temp/package.json
+cp package-lock.json docker-temp/package-lock.json
+cp dockerfile docker-temp/dockerfile
+cp -r dist docker-temp/dist
+
+docker build docker-temp
