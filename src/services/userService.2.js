c92fcfea9fed567ee7a1a774a86cb98f2a04c10d
Luke Kaalim <lukekaalim@gmail.com>
2019-08-27 18:12:09 +1000

    Update user service to return a Result

--- src/services/userService.2.js
+++ src/services/userService.2.js
@@ -1,11 +1,23 @@
 // @flow
 const { createUser } = require('../models/user');
+const { succeed, fail } = require('../lib/result');
 /*::
+import type { Result } from '../lib/result';
 import type { User, UserID } from '../models/user';
 import type { Storage } from './storage.2';
+*/
 
+class StorageError extends Error {
+  internalError/*: Error*/;
+  constructor(internalError/*: Error*/) {
+    super(`${internalError.message}\nThere was an error in the User Service Storage`);
+    this.internalError = internalError;
+  }
+}
+
+/*::
 export type UserService = {
-  getAllUsers: () => Promise<Array<User>>,
+  getAllUsers: () => Promise<Result<Array<User>, StorageError>>,
   addUser: () => Promise<User>,
   deleteUser: (userId: UserID) => Promise<void>,
 };
@@ -16,9 +28,13 @@ const createUserService = (
   userStorage/*: Storage<UserID, User>*/,
 )/*: UserService*/ => {
   const getAllUsers = async () => {
-    const userIds = await userIdStorage.read(null);
-    const users = await Promise.all(userIds.map(id => userStorage.read(id)));
-    return users;
+    try {
+      const userIds = await userIdStorage.read(null);
+      const users = await Promise.all(userIds.map(id => userStorage.read(id)));
+      return succeed(users);
+    } catch (internalError) {
+      return fail(new StorageError(internalError));
+    }
   };
   const addUser = async () => {
     const user = createUser();
