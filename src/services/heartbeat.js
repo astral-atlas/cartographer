852afc05faf4b5796b4e56dd8c3cdbca3b60224e
Luke Kaalim <lukekaalim@gmail.com>
2019-08-27 18:09:12 +1000

    implement heartbeat service

---
+++ src/services/heartbeat.js
@@ -0,0 +1,25 @@
+// @flow
+/*::
+import type { EventLogger } from './log.2';
+*/
+const { heartbeat } = require('../events/heartbeatEvents');
+
+const createHeartbeat = (logger/*: EventLogger*/, beatLength/*: number*/ = 1000) => {
+  const onBeat = () => {
+    logger.log(heartbeat(process.memoryUsage()));
+  };
+
+  const intervalId = setInterval(onBeat, beatLength);
+
+  const stop = () => {
+    clearInterval(intervalId);
+  };
+
+  return {
+    stop,
+  };
+}
+
+module.exports = {
+  createHeartbeat,
+};
