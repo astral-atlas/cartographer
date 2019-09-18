291a2deed883f09d6ffb5e4b069075f66ca8ff57
Luke Kaalim <lkaalim@nine.com.au>
2019-09-18 23:24:15 +1000

    add delete session contract test

--- src/routes/sessionRoutes.contract.test.js
+++ src/routes/sessionRoutes.contract.test.js
@@ -2,11 +2,12 @@
 const { readFile } = require('fs').promises;
 const { join } = require('path');
 const { contractModel, withContractClient } = require('@lukekaalim/contract');
-const { expect, assert } = require('@lukekaalim/test');
+const { expect, assert, expectAll } = require('@lukekaalim/test');
 const { createServer } = require('http');
 const { createListener, notFound } = require('@lukekaalim/server');
 
 const { createUser } = require('../models/user');
+const { createSession } = require('../models/session');
 
 const { createCartographer } = require('../cartographer');
 const { createSessionRoutes } = require('./sessionRoutes');
@@ -33,12 +34,12 @@ const withHttpServer = async (listener, serverHandler) => {
   }
 };
 
-const expectSessionContract = (topographPath/*: string*/) => expect(async () => {
+const expectGetSessionContract = (topographPath) => expect(async () => {
   const contractFile = await readFile(join(topographPath, 'session', 'getSession.json'), 'utf-8');
   const contract = resultOrThrow(contractModel.from(JSON.parse(contractFile)));
 
   const config = {
-    authentication: { type: 'fixed', name: 'luke', pass: '123', userId: createUser().id },
+    authentication: { type: 'none' },
     cors: { origins: [] },
     port: 0,
     storage: { type: 'memory' }
@@ -57,8 +58,43 @@ const expectSessionContract = (topographPath/*: string*/) => expect(async () => 
   const contractResult = await serverResult.success;
   if (contractResult.type === 'failure') return assert(contractResult.failure.message, false);
 
-  return assert('It worked?', contractResult.type === 'success');
+  return assert('Sessions should fulfil the getSession contract', contractResult.type === 'success');
 });
+
+const expectDeleteSessionContract = (topographPath) => expect(async () => {
+  const contractFile = await readFile(join(topographPath, 'session', 'deleteSession.json'), 'utf-8');
+  const contract = resultOrThrow(contractModel.from(JSON.parse(contractFile)));
+
+  const config = {
+    authentication: { type: 'none' },
+    cors: { origins: [] },
+    port: 0,
+    storage: { type: 'memory' }
+  };
+  const memoryMap = createMemoryMapStore();
+  const logger = createMockLogger();
+  const session = createSession('The End', 100);
+  session.id = '123';
+  memoryMap.write(session.id, session);
+  const userService = createSessionService(memoryMap);
+  const listener = createListener(createSessionRoutes(logger, config, userService), () => notFound());
+
+  const serverResult = await withHttpServer(listener, ({ port }) => {
+    return withContractClient(contract, `http://localhost:${port}`, client => {
+      return client.execute();
+    });
+  });
+  if (serverResult.type === 'failure') return assert(serverResult.failure.message, false);
+  const contractResult = await serverResult.success;
+  if (contractResult.type === 'failure') return assert(contractResult.failure.message, false);
+
+  return assert('Sessions should fulfil the deleteSession contract', contractResult.type === 'success');
+});
+
+const expectSessionContract = (topographPath/*: string*/) => expectAll('Sessions should fulfil contracts from topograph', [
+  expectDeleteSessionContract(topographPath),
+  expectGetSessionContract(topographPath)
+]);
 
 module.exports = {
   expectSessionContract,
